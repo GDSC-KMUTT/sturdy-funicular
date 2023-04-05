312553e68ad8d1a2e762d33687485677f5628153
koranat <guyza01.2547@gmail.com>
2023-04-05 19:10:13 +0700

    fix: wrong base name

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/supreme-succotash/",
+  base: "/sturdy-funicular/",
 });
